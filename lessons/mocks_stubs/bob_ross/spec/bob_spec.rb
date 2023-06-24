a029aaec97709f009b8c8863353b5357e545d09f
Jennifer Rush <jennifer.rush2@gmail.com>
2023-06-24 11:19:49 -0600

    Complete Bob Ross lesson

--- lessons/mocks_stubs/bob_ross/spec/bob_spec.rb
+++ lessons/mocks_stubs/bob_ross/spec/bob_spec.rb
@@ -4,13 +4,13 @@ require './lib/paint'
 
 RSpec.describe Bob do
   describe 'Instantiation' do
-    it 'exists' do
+    xit 'exists' do
       bob = Bob.new
 
       expect(bob).to be_an_instance_of(Bob)
     end
 
-    it 'starts with no paints' do
+    xit 'starts with no paints' do
       bob = Bob.new
 
       expect(bob.paints).to eq([])
@@ -18,7 +18,7 @@ RSpec.describe Bob do
   end
 
   describe 'Methods' do
-    it 'can add paint' do
+    xit 'can add paint' do
       bob = Bob.new
       paint_1 = Paint.new("Alizarin Crimson")
       paint_2 = Paint.new("Van Dyke Brown")
@@ -29,7 +29,7 @@ RSpec.describe Bob do
       expect(bob.paints).to eq([paint_1, paint_2])
     end
 
-    it 'can return paint colors' do
+    xit 'can return paint colors' do
       bob = Bob.new
       paint_1 = Paint.new("Alizarin Crimson")
       paint_2 = Paint.new("Van Dyke Brown")
@@ -41,8 +41,15 @@ RSpec.describe Bob do
 
     it 'can calculate total paint amount' do
       bob = Bob.new
-      paint_1 = Paint.new("Alizarin Crimson", 42)
-      paint_2 = Paint.new("Van Dyke Brown", 25)
+      paint_1 = double("first paint")
+      paint_2 = double("second paint")
+
+      allow(paint_1).to receive(:amount).and_return(42)
+      allow(paint_2).to receive(:amount).and_return(25)
+
+
+      #paint_1 = Paint.new("Alizarin Crimson", 42)
+      #paint_2 = Paint.new("Van Dyke Brown", 25)
 
       bob.add_paint(paint_1)
       bob.add_paint(paint_2)
