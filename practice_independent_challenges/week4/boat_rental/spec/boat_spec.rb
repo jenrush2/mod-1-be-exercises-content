3db14cad9e60a44fac4fd83c76c06fcf16d4f261
Jennifer Rush <jennifer.rush2@gmail.com>
2023-06-25 12:19:39 -0600

    Create boat class and test

---
+++ practice_independent_challenges/week4/boat_rental/spec/boat_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/boat'
+require './lib/renter'
+
+RSpec.describe Boat do
+    it 'exists' do
+        kayak = Boat.new(:kayak, 20)
+
+        expect(kayak).to be_an_instance_of(Boat)
+    end
+
+    it 'has attributes' do
+        kayak = Boat.new(:kayak, 20)
+
+        expect(kayak.type).to eq :kayak
+        expect(kayak.price_per_hour).to eq 20
+        expect(kayak.hours_rented).to eq 0
+    end
+
+    it 'can add hours' do
+        kayak = Boat.new(:kayak, 20)
+
+        kayak.add_hour
+        kayak.add_hour
+        kayak.add_hour
+
+        expect(kayak.hours_rented).to eq 3
+    end
+
+end
