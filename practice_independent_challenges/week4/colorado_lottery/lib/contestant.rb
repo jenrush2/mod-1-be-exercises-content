9f97c1d69a31fc7b6e85acf5ca838fa81b8288e3
Jennifer Rush <jennifer.rush2@gmail.com>
2023-06-26 12:07:25 -0600

    Make Contestant attributes test pass

--- practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
@@ -1,6 +1,17 @@
 require './lib/game'
 
 class Contestant
+    attr_reader :first_name, 
+                :last_name,
+                :age,
+                :state_of_residence,
+                :spending_money
+
     def initialize(args)
+        @first_name = args[:first_name]
+        @last_name = args[:last_name]
+        @age = args[:age]
+        @state_of_residence = args[:state_of_residence]
+        @spending_money = args[:spending_money]
     end
 end
