b38f85e8c70344b2132e1c38c5a2f27a0163351e
Jennifer Rush <jennifer.rush2@gmail.com>
2023-06-26 12:09:07 -0600

    Create Contestant can give full name test

--- practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
@@ -26,4 +26,15 @@ RSpec.describe Contestant do
         expect(alexander.state_of_residence).to eq 'CO'
         expect(alexander.spending_money).to eq 10
     end
+
+    it 'can give the full name' do
+        alexander = Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})
+        
+        expect(alexander.full_name).to eq "Alexander Aigiades"
+    end
+
 end
