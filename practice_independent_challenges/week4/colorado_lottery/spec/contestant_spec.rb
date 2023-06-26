ebaffe48f14cb8e6117e0698bc863743cc7926d6
Jennifer Rush <jennifer.rush2@gmail.com>
2023-06-26 12:05:07 -0600

    Make test for Contestant attributes

--- practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Contestant do
 
         expect(alexander).to be_an_instance_of Contestant
     end
+
+    it 'has attributes' do
+        alexander = Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})
+
+        expect(alexander.first_name).to eq 'Alexander'
+        expect(alexander.last_name).to eq 'Aigiades'
+        expect(alexander.age).to eq 28
+        expect(alexander.state_of_residence).to eq 'CO'
+        expect(alexander.spending_money).to eq 10
+    end
 end
