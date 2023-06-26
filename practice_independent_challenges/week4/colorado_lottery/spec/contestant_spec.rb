36b537be669da34a6484d4335ec173fa384bd884
Jennifer Rush <jennifer.rush2@gmail.com>
2023-06-26 12:12:08 -0600

    Create Contestant can test for out of state test

--- practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
@@ -37,4 +37,14 @@ RSpec.describe Contestant do
         expect(alexander.full_name).to eq "Alexander Aigiades"
     end
 
+    it 'can test for out of state' do
+        alexander = Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})
+        
+        expect(alexander.out_of_state?).to eq false
+    end
+
 end
